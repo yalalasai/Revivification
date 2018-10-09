9cbe3010fb81235c59ac8fa494c9222cfcbb7657
Y.Sai Krishna <saikrishnayalala2791@gmail.com>
2018-10-09 18:23:11 +0530

    Update Helloworld.java
    test

--- Helloworld.java
+++ Helloworld.java
@@ -1,9 +1,9 @@
-import java.util.*:
+import java.util.*;
 
 public class Helloworld
 {
 	public static void main(String[]args)
 	{
-	 Sytem.out.println("Hello world);
+	 Sytem.out.println("Hello world");
 	}
 }
